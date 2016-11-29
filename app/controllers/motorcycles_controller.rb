2c379526be2f913c0d7de2bf87d08fbbe68fb422
Sebastiaan van Kuyk <sebastiaan.vankuyk@gmail.com>
2016-11-29 16:43:54 +0100

    show action and view done

--- app/controllers/motorcycles_controller.rb
+++ app/controllers/motorcycles_controller.rb
@@ -1,8 +1,19 @@
 class MotorcyclesController < ApplicationController
+
+  before_action :set_motorcycle, only: :show
+
   def index
     @motorcycles = Motorcycle.all
   end
 
   def show
+
   end
+
+private
+
+  def set_motorcycle
+    @motorcycle = Motorcycle.find(params[:id])
+  end
+
 end
