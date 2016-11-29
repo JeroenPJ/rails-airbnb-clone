57a4a5e1fd5e018dda6922399a47c5ff904c0d9d
Arthur Garaud <arthurgaraud@gmail.com>
2016-11-29 14:09:11 +0100

    create reservation model update

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,5 +2,5 @@ class Reservation < ApplicationRecord
   belongs_to :motorcycle
   belongs_to :user
 
-  validates :starting_date, :ending_date
+  validates :starting_date, :ending_date, presence: true
 end
