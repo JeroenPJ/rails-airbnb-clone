27e973f548586c5acc844fa1f7a8b1c160f3945b
Arthur Garaud <arthurgaraud@gmail.com>
2016-12-01 17:40:21 +0100

    daterangepicker with reservations filtered out working, ugly

--- app/controllers/motorcycles_controller.rb
+++ app/controllers/motorcycles_controller.rb
@@ -6,14 +6,14 @@ class MotorcyclesController < ApplicationController
 
   def index
     @available_motorcycles = []
-    location = params[:location]
-    date = params[:daterange].split(" - ").map {|date| Date.strptime(date,"%m/%d/%Y")}
-    @motorcycles = Motorcycle.near(location, 50)
+    @location = params[:location]
+    @date = params[:daterange].split(" - ").map {|date| Date.strptime(date,"%m/%d/%Y")}
+    @motorcycles = Motorcycle.near(@location, 50)
 
     @motorcycles.each do |motorcycle|
       overlaps = false
       motorcycle.reservations.each do |reservation|
-        overlaps = conflict?(reservation, date)
+        overlaps = conflict?(reservation, @date)
         break if overlaps
       end
       @available_motorcycles << motorcycle unless overlaps
@@ -30,7 +30,8 @@ class MotorcyclesController < ApplicationController
     @reservation = Reservation.new
     @starting_date = Date.strptime(params[:starting_date],"%Y-%m-%d")
     @ending_date = Date.strptime(params[:ending_date],"%Y-%m-%d")
-    @overall_price = @motorcycle.price * Date.strptime(params[:ending_date],"%Y-%m-%d") - Date.strptime(params[:starting_date],"%Y-%m-%d")
+    @overall_price = (@motorcycle.price.to_f) * (Date.strptime(params[:ending_date],"%Y-%m-%d") - Date.strptime(params[:starting_date],"%Y-%m-%d")).to_f
+
   end
 
   def new
